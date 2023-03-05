15a3092fc20d9308c62287c1bc3264b110e87554
x3rim <lordpunk4@gmail.com>
2023-03-05 23:17:57 +0300

    Homework

---
+++ 4.8 Homework task 1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._8_Homework_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Задание 1.Случайная матрица
+            //Что нужно сделать
+            //Выведите на экран матрицу заданного размера и заполните её случайными числами.Детальный алгоритм работы приложения:
+
+            //Сначала пользователю предлагается ввести количество строк в будущей матрице.
+            //Затем — ввести количество столбцов в будущей матрице.
+            //Когда данные будут получены, нужно создать в памяти матрицу заданного размера.
+            //Используя Random, заполнить матрицу случайными целыми числами.
+            //Вывести полученную матрицу на экран.
+            //Вывести сумму всех элементов этой матрицы на экран отдельной строкой.
+            //Рекомендация
+            //Для работы с матрицами используйте вложенные циклы.
+
+            //Что оценивается
+            //Программа выводит на экран случайно созданную матрицу предварительно заданного размера, а также сумму всех элементов в ней.
+
+            Console.WriteLine("Случайная матрица");
+            Console.WriteLine("Введите желаемое количество строк и столбцов!\n");
+
+            Console.Write("Введите количество строк: ");
+            int line = int.Parse(Console.ReadLine());
+
+            Console.Write(("Введите количество столбцов: "));
+            int columns = int.Parse(Console.ReadLine());
+
+            int amountOfElements = line * columns;
+
+            int[,] array = new int[line, columns];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < line; i++)
+                {
+                for (int j = 0; j < columns; j++)
+                    {
+                        array[i, j] = rand.Next(10);
+                        Console.Write($" {array[i, j]}");
+                    }
+                Console.WriteLine();
+                }
+                Console.Write($"\nКоличество элементов: {amountOfElements}");
+
+                Console.ReadKey();
+        
+
+        }
+    }
+}
---
+++ 4.8 Homework task 2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._8_Homework_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Задание 2.Сложение матриц
+            //Что нужно сделать
+            //Используя размеры матрицы из предыдущего задания, создайте ещё одну матрицу.Сложите две матрицы.
+
+            //Сложение матриц А и В — это нахождение такой матрицы С, все элементы которой представляют собой сложенные попарно соответствующие элементы исходных матриц А и В.
+            //Складывать допускается только матрицы одинаковой размерности(допустим, m × n), то есть имеющие равное количество строк и равное количество столбцов.
+
+            //Таким образом, математически сумма матриц выглядит так:
+
+            //Аm×n + Bm×n = Cm×n
+
+            //Каждый элемент искомой матрицы равен сумме соответствующих элементов заданных матриц:
+
+            //            cij = aij + bij,
+
+            //где i принимает значение от 1 до m, j имеет значение от 1 до n.
+
+            //Рассмотрим пример сложения двух матриц размера 2 × 3.
+
+            //Даны две матрицы:
+
+
+
+            //            Необходимо найти сумму матриц А и В.
+
+            //            Решение:
+
+
+
+            //Выведите на экран полученную матрицу.
+
+            //Что оценивается
+            //Программа выводит на экран две случайные матрицы и их сумму.
+
+
+            Console.WriteLine("Случайная матрица");
+            Console.WriteLine("Введите желаемое количество строк и столбцов!\n");
+
+            Console.Write("Введите количество строк: ");
+            int line = int.Parse(Console.ReadLine());
+
+            Console.Write(("Введите количество столбцов: "));
+            int columns = int.Parse(Console.ReadLine());
+
+            int[,] arrayA = new int[line, columns];
+
+            Random randA = new Random();
+
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arrayA[i, j] = randA.Next(10);
+                    Console.Write($" {arrayA[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            int[,] arrayB = new int[line, columns];
+
+            Random randB = new Random();
+
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arrayB[i, j] = randB.Next(10);
+                    Console.Write($" {arrayB[i, j]}");
+                }
+                Console.WriteLine();
+            }   
+                Console.WriteLine();
+                Console.WriteLine();
+
+                int[,] arrayC = new int[line, columns];
+
+                for (int n = 0; n < line; n++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        arrayC[n, j] = arrayA[n, j] + arrayB[n, j];
+                        Console.Write($" {arrayC[n, j]}");
+                    }
+
+                    Console.WriteLine();
+                              
+                }
+            
+
+            Console.ReadKey();
+        }
+
+    }
+}
